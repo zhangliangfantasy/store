68304b6615ad2f3c22e2c36583de14f982fcf428
fantashzl <1106401562@qq.com>
2019-12-22 23:13:05 +0800

    Add icon component and homepage layout component.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header></Header>
+    <router-view />
   </div>
 </template>
 
 <script>
+import Header from '@/components/header/Header'
 export default {
-  name: 'App'
+  name: 'APP',
+  components: {
+    Header
+  }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
